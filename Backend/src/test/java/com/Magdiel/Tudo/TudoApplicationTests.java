cc795c8938f79e89d5ce7153030eb37897449daf
Magdiel-Mendes <santosmagdiel931@outlook.pt>
2021-11-24 09:29:54 -0300

    commit inicial

---
+++ Backend/src/test/java/com/Magdiel/Tudo/TudoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.Magdiel.Tudo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TudoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
